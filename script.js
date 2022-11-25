75fd49bd935ddc827c38403ecae1efa0ae75735c
Lance Ezeckel P. Fernando <lanceezeckel.fernando-20@cpu.edu.ph>
2022-11-25 20:24:32 +0800

    Final Result

---
+++ script.js
@@ -0,0 +1,26 @@
+function toggleHTML() {
+  var x = document.getElementById("html");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function toggleCSS() {
+  var x = document.getElementById("css");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function toggleJavascript() {
+  var x = document.getElementById("javascript");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
